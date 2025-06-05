e5957e7d961dc11b54895d48144cd63bf9cf2541
Santiago Dominguez <santiagodominguezarj@gmail.com>
2025-06-05 20:16:52 -0300

    Ejercicio 1 y arreglos de carpetas

--- Ejercicio1/Program.cs
+++ Ejercicio1/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string s;
+Console.WriteLine("Ingrese un numerox:");
+s = Console.ReadLine();
+int num;
+bool resultado =int.TryParse(s, out num);
+if (resultado)
+{
+    if (num > 0)
+    {
+        float inversion = 1f / num;
+        Console.WriteLine($"El resultado de 1/{num}  es:{inversion}");
+    }
+    else
+    {
+        Console.WriteLine("Ingrese un numero mayor que 0");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada no valida");
+}
