27553da24f7479e58f30d7983a6af82410733ee0
Santiago Dominguez <santiagodominguezarj@gmail.com>
2025-06-10 19:27:33 -0300

    Completado el Control del Proximo menu

--- Ejercicio2y3/Program.cs
+++ Ejercicio2y3/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ingrese la operacion que desea Realizar");
+int operacion;
+string numOp;
+int status=1;
+do
+{
+
+    Console.WriteLine("Ingrese la operacion que desea Realizar");
+    numOp = Console.ReadLine();
+    bool test = int.TryParse(numOp, out operacion);
+    if (!test)
+    {
+        Console.WriteLine("Ingrese un Numero Valido");
+    }
+} while (operacion <= 0 || operacion >= 5);
