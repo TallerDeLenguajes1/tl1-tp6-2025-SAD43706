e0c93ff1fa8700843f4c034611ebc18def580b1b
Santiago Dominguez <santiagodominguezarj@gmail.com>
2025-06-16 14:38:53 -0300

    Modulacion de operaciones y completado el switch

--- Ejercicio2y3/Program.cs
+++ Ejercicio2y3/Program.cs
@@ -1,45 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int operacion, num1, num2, suma, resta, multi;
-float divi;
-string numOp,nume1,nume2;
+int operacion, num1, num2;
+string numOp,nume1,nume2,deno;
 char status = 's';
 do
 {
-
-    Console.WriteLine("Ingrese la operacion que desea Realizar");
-    numOp = Console.ReadLine();
-    bool test1 = int.TryParse(numOp, out operacion);
-    if (!test1)
+    do
     {
-        Console.WriteLine("Ingrese un Numero Valido");
-    }
-} while (operacion <= 0 || operacion >= 5);
-do
-{
-    Console.WriteLine("Ingrese los Numeros:");
-    nume1 = Console.ReadLine();
-    nume2 = Console.ReadLine();
-    int.TryParse(nume1, out num1);
-    int.TryParse(nume2, out num2);
-    if (!int.TryParse(nume1, out num1) && !int.TryParse(nume2, out num2))
+        Console.WriteLine("Ingrese la operacion que desea Realizar");
+        numOp = Console.ReadLine();
+        bool test1 = int.TryParse(numOp, out operacion);
+        if (!test1)
+        {
+            Console.WriteLine("Ingrese un Numero Valido");
+        }
+    } while (operacion <= 0 || operacion >= 5);
+    do
     {
-        Console.WriteLine("☺ INGRESE NUMEROS ☺");
+        Console.WriteLine("Ingrese los Numeros:");
+        nume1 = Console.ReadLine();
+        nume2 = Console.ReadLine();
+        int.TryParse(nume1, out num1);
+        int.TryParse(nume2, out num2);
+        if (!int.TryParse(nume1, out num1) && !int.TryParse(nume2, out num2))
+        {
+            Console.WriteLine("☺ INGRESE NUMEROS ☺");
+        }
+    } while (!int.TryParse(nume1, out num1) && !int.TryParse(nume2, out num2));
+    switch (operacion)
+    {
+        case 1:
+            Console.WriteLine($"{suma(num1, num2)}");
+            break;
+        case 2:
+            Console.WriteLine($"{resta(num1, num2)}");
+            break;
+        case 3:
+            Console.WriteLine($"{multiplicacion(num1, num2)}");
+            break;
+        case 4:
+            Console.WriteLine($"{division(num1, num2)}");
+            break;
+        default:
+            break;
     }
-} while (!int.TryParse(nume1, out num1) && !int.TryParse(nume2, out num2));
+    Console.WriteLine("Desea realizar otra operacion(s/n)");
+    status = Console.ReadLine();
+} while (status == 's');
 
-switch (operacion)
+
+float division(int num1,int num2)
 {
-    case 1:
-        suma = num1 + num2;
-        Console.WriteLine($"La suma de {num1} + {num2} es igual a {suma}");
-        break;
-    case 2:
-        resta = num1 - num2;
-        break;
-    case 3:
-        break;
-    case 4:
-        break;
-    default:
-        break;
+    if (num2 == 0)
+    {
+        Console.WriteLine("!!EL DENOMINADOR NO PUEDE SER 0!!");
+        do
+        {
+            Console.WriteLine("Igrese nuevamente el denominador:");
+            deno = Console.ReadLine();
+            bool test4 = int.TryParse(deno, out num2);
+            if (!test4)
+            {
+                Console.WriteLine("Ingrese un Numero Valido");
+            }
+        } while (num2 == 0);
+    }
+    return num1 / (float)num2;
+}
+int multiplicacion(int num1, int num2)
+{
+    return num1 * num2;
+}
+int resta(int num1,int num2)
+{
+    return num1 - num2;
+}
+int suma(int num1,int num2)
+{
+        return num1 + num2;
 }
