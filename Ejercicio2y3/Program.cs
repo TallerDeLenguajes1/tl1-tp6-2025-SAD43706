dd939ac6456b6f8ef3a2523a4b052f988a29c7bc
Santiago Dominguez <santiagodominguezarj@gmail.com>
2025-06-26 10:48:50 -0300

    calculadoraV2 completa

--- Ejercicio2y3/Program.cs
+++ Ejercicio2y3/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int operacion, num1, num2;
-string numOp,nume1,nume2,deno;
+using System;
+int operacion;
+float num1,num2,num3;
+string numOp,nume1,nume2,deno,nume3;
 char status = 's';
 do
 {
@@ -13,32 +15,60 @@ do
         {
             Console.WriteLine("Ingrese un Numero Valido");
         }
-    } while (operacion <= 0 || operacion >= 5);
-    do
+    } while (operacion <= 4 || operacion >= 13);
+    if (operacion<=6)
     {
-        Console.WriteLine("Ingrese los Numeros:");
-        nume1 = Console.ReadLine();
-        nume2 = Console.ReadLine();
-        int.TryParse(nume1, out num1);
-        int.TryParse(nume2, out num2);
-        if (!int.TryParse(nume1, out num1) && !int.TryParse(nume2, out num2))
+        do
         {
-            Console.WriteLine("☺ INGRESE NUMEROS ☺");
-        }
-    } while (!int.TryParse(nume1, out num1) && !int.TryParse(nume2, out num2));
+            Console.WriteLine("Ingrese los Numeros:");
+            nume1 = Console.ReadLine();
+            nume2 = Console.ReadLine();
+            int.TryParse(nume1, out num1);
+            int.TryParse(nume2, out num2);
+            if (!int.TryParse(nume1, out num1) && !int.TryParse(nume2, out num2))
+            {
+                Console.WriteLine("☺ INGRESE NUMEROS ☺");
+            }
+        }  while (!int.TryParse(nume1, out num1) && !int.TryParse(nume2, out num2));
+    }else
+    {
+        do
+        {
+            Console.WriteLine("Ingrese el Numero:");
+            nume3 = Console.ReadLine();
+            float.TryParse(nume3, out num3);
+            if (!float.TryParse(nume3, out num3))
+            {
+                Console.WriteLine("☺ INGRESE UN NUMERO ☺");
+            }
+        } while (!float.TryParse(nume3, out num3));
+    }
+    
     switch (operacion)
     {
-        case 1:
-            Console.WriteLine($"{suma(num1, num2)}");
+        case 5:
+            Console.WriteLine($"{maximo(num1,num2)}");
             break;
-        case 2:
-            Console.WriteLine($"{resta(num1, num2)}");
+        case 6:
+            Console.WriteLine($"{minimo(num1,num2)}");
             break;
-        case 3:
-            Console.WriteLine($"{multiplicacion(num1, num2)}");
+        case 7:
+            Console.WriteLine($"{valorAbs(num3)}");
             break;
-        case 4:
-            Console.WriteLine($"{division(num1, num2)}");
+        case 8:
+            Console.WriteLine($"{cuadrado(num3)}");
+            break;
+        case 9:
+            Console.WriteLine($"{raiz(num3)}");
+            break;
+        case 10:
+            Console.WriteLine($"{seno(num3)}");
+            break;
+        case 11:
+            Console.WriteLine($"{coseno(num3)}");
+            break;
+        case 12:
+            Console.WriteLine($"{parteEnt(num3)}");
             break;
         default:
             break;
@@ -48,33 +78,49 @@ do
 } while (status == 's');
 
 
-float division(int num1,int num2)
+float valorAbs(float num3)
 {
-    if (num2 == 0)
+    return Math.Abs(num3);
+}
+float cuadrado(float num3)
+{
+    return Math.Pow(num3,2);
+}
+float raiz(float num3)
+{
+    return Math.Sqrt(num3);
+}
+float seno(float num3)
+{
+    return Math.sin(num3);
+}
+float coseno(float num3)
+{
+    return Math.Cos(num3);
+}
+int parteEnt()
+{
+    return (int)num3;
+}
+float maximo(float max1,float max2)
+{
+    if (max1 > max2)
     {
-        Console.WriteLine("!!EL DENOMINADOR NO PUEDE SER 0!!");
-        do
-        {
-            Console.WriteLine("Igrese nuevamente el denominador:");
-            deno = Console.ReadLine();
-            bool test4 = int.TryParse(deno, out num2);
-            if (!test4)
-            {
-                Console.WriteLine("Ingrese un Numero Valido");
-            }
-        } while (num2 == 0);
+        return printf("El maximo es: %f", max1);
     }
-    return num1 / (float)num2;
+    else
+    {
+        return printf("El maximo es: %f", max2);
+    }
 }
-int multiplicacion(int num1, int num2)
+float minimo(float min1,float min2)
 {
-    return num1 * num2;
-}
-int resta(int num1,int num2)
-{
-    return num1 - num2;
-}
-int suma(int num1,int num2)
-{
-        return num1 + num2;
+    if (max1 < max2)
+    {
+        return printf("El minimo es: %f", max1);
+    }
+    else
+    {
+        return printf("El minimo es: %f", max2);
+    }
 }
